18fb5c6f80f43004d671eeb566e85ad4f9249eb6
Niklas Mencke <niklas.mencke@googlemail.com>
2022-04-08 13:13:22 +0200

    added small additionally test

---
+++ src/RestApp.tsx
@@ -0,0 +1,18 @@
+/*
+ * This is a GET endpoint, that returns a random Kanye West Quote
+ * Format: {"quote":"This is a random quote"}
+ */
+const QUOTE_API_URL = "https://api.kanye.rest";
+
+const fetchRandomQuote = async () => {}; // Implement this function
+
+/*
+ * Implement a simple app, that displays a random Kanye West quote on the click of a button.
+ * Use the QUOTE_API_URL above to request data via GET.
+ * Use Hooks for your implementation.
+ */
+const RestApp = () => {
+  return null;
+};
+
+export default RestApp;
