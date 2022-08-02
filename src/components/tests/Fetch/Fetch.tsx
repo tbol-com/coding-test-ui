aa02708669c9acca7355bf81593f704fda5a2e59
lus21 <lus21484@gmail.com>
2022-08-02 15:53:27 +0400

    add urls to the component files

--- src/components/tests/Fetch/Fetch.tsx
+++ src/components/tests/Fetch/Fetch.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Fetch.css";
 
+const CHARACTERS_API_URL = "https://rickandmortyapi.com/api/character";
+
 const Fetch: React.FC = () => {
   return <h1 className="fetchTitle">Fetch Component</h1>;
 };
--- src/components/tests/Form/Form.tsx
+++ src/components/tests/Form/Form.tsx
@@ -1,5 +1,8 @@
+import { timingSafeEqual } from "crypto";
 import React from "react";
 import "./Form.css";
+
+const FORM_SUBMIT_URL = "https://fakestoreapi.com/products";
 
 const Form: React.FC = () => {
   return <h1 className="formTitle">Form Component</h1>;
